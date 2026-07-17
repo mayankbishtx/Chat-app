e58e63bbdb36a35340e2f1d892f877d1a1547865
mayankbisht01 <mayankrajput2480@gmail.com>
2026-07-17 22:01:52 +0530

    deployment configuration

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -4,7 +4,7 @@ let socket: Socket | null = null;
 
 export function getSocket(): Socket {
     if (!socket) {
-        socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:4000");
+        socket = io(process.env.BACKEND_URL || "http://localhost:4000");
     }
     return socket;
 }
